5ee60b766c42bd73f4d700ed21d5e9969e987b43
GeoHolz <nourssh@gmail.com>
2018-02-21 12:10:23 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 $NEED_TO_LOG_FOR_ACCESS = false;
 $REGISTER_OPEN = false;
 $MYSQL_HOST="localhost";
-$MYSQL_DB="BTZ";
-$MYSQL_USER="BTZ_user";
-$MYSQL_PASS="MdpBTZ";
-$EMAIL="geoholz.alerte@gmail.com";
+$MYSQL_DB="";
+$MYSQL_USER="";
+$MYSQL_PASS="";
+$EMAIL="";
 include "functions.php";
 ?>
