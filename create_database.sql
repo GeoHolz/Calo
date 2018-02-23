571e04f41fc61236b49416994c1e3436d97b3f91
geoholz <geoholz@debian8>
2018-02-23 15:05:38 +0100

    Add firstname / lastname

---
+++ create_database.sql
@@ -0,0 +1,21 @@
+-- Adminer 4.5.0 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+CREATE DATABASE `BTZ` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `BTZ`;
+
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE `members` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(40) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `date_signup` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2018-02-23 14:04:44
